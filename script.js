8be36928bf3194bb3fac6cd0f231858e46c9ef5d
qqboy-source <53020240+qqboy-source@users.noreply.github.com>
2025-02-24 23:38:58 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 // Here for Javascript
+// Fade-in on scroll
+        const faders = document.querySelectorAll('.fade-in-on-scroll');
+
+        const appearOptions = {
+            threshold: 0.5 // Adjust as needed
+        };
+
+        const appearOnScroll = new IntersectionObserver(function (entries, observer) {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add('active');
+                    observer.unobserve(entry.target);
+                } else {
+                    entry.target.classList.remove('active');  // Remove 'active' class on scroll up
+                }
+            })
+        }, appearOptions);
+
+        faders.forEach(fader => {
+            appearOnScroll.observe(fader);
+        });
